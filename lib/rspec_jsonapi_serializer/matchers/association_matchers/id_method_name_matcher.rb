1288f38bccf41ac34b861f3ddde3ec1b502a07ff
Mateus Cruz <makteus@gmail.com>
2023-05-20 21:37:35 -0400

    Add descriptions for relationship id_method_name submatcher

--- lib/rspec_jsonapi_serializer/matchers/association_matchers/id_method_name_matcher.rb
+++ lib/rspec_jsonapi_serializer/matchers/association_matchers/id_method_name_matcher.rb
@@ -9,7 +9,7 @@ module RSpecJSONAPISerializer
         def initialize(value, relationship_target)
           super(value)
 
-          @relationship_target  = relationship_target
+          @relationship_target = relationship_target
         end
 
         def matches?(serializer_instance)
@@ -18,17 +18,17 @@ module RSpecJSONAPISerializer
           actual == expected
         end
 
-        def main_failure_message
-          [expected_message, actual_message].compact.join(", ")
+        def description
+          "with id method name #{expected}"
+        end
+
+        def expectation
+          [ "with id method name #{expected}", actual_message ].compact.join(", ")
         end
 
         private
 
         attr_reader :relationship_target
-
-        def expected_message
-          "expected #{serializer_name} to use #{expected} as id_method_name for #{relationship_target}"
-        end
 
         def actual_message
           actual ? "got #{actual} instead" : nil
